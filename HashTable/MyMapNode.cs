19f4613c9d644ea73e2b7a6786401cf5b2441d19
eswar <neelaveni.eswar04@gmail.com>
2022-04-25 15:40:26 +0530

    [Eswar]Ability to find frequency of words in a sentence

---
+++ HashTable/MyMapNode.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    public class MyMapNode<K, V>
+    {
+        private readonly int size;
+        private readonly LinkedList<KeyValue<K, V>>[] items;
+
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+        protected int GetArrayPosition(K key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+        public V Get(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(position);
+            foreach (KeyValue<K, V> iten in linkedlist)
+            {
+                if (iten.key.Equals(key))
+                {
+                    return iten.value;
+                }
+            }
+            return default(V);
+        }
+        public void Add(K key,V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K,V>> linkedList = GetLinkedList(position);
+            KeyValue<K, V> iten = new KeyValue<K, V>() { key =key, value =value };
+            linkedList.AddLast(iten);
+        }
+        public void Remove(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            bool itenFound = false;
+            KeyValue<K, V> foundIten = default(KeyValue<K, V>);
+            foreach (KeyValue<K, V> iten in linkedList)
+            {
+                if (iten.key.Equals(key))
+                {
+                    itenFound = true;
+                    foundIten = iten;
+                }
+            }
+            if (itenFound)
+            {
+                linkedList.Remove(foundIten);
+            }
+                
+        }
+        protected LinkedList<KeyValue<K, V>> GetLinkedList (int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedlist = items[position];
+            if (linkedlist == null)
+            {
+                linkedlist = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedlist;
+            }
+            return linkedlist;
+        }
+    }
+    public struct KeyValue<K, V>
+    {
+        public K key { get; set; }
+        public V value { get; set; }
+    }  
+        }
+        
+
+    
+
--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -11,8 +11,23 @@ namespace HashTable
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Hash Table");
+
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "To");
+            hash.Add("5", "be");
+            string hash5 = hash.Get("5");
+            Console.WriteLine("5 th index value :" + hash5);
+            //hash .Remove("2");
+            string hash2 = hash.Get("2");
+            Console.WriteLine("2 nd index value :" + hash2);
+
             Console.ReadLine();
         }
+        
 
     }
 }
